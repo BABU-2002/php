1a1d9f120413dffad3393f2da4a9490dd84be15b
BABU ELANGO <119952884+BABU-2002@users.noreply.github.com>
2023-03-04 17:28:20 +0530

    Add files via upload

---
+++ github.php
@@ -0,0 +1,43 @@
+<html>
+    <head>
+        
+    </head>
+<body align="center">
+
+    <form   name="form"   method="POST">
+        <div>
+        ENTER YOUR NAME:
+        <input id="name" name="name" placeholder="Enter your name" type="text">
+    </div>
+<div>
+        <br> ENTER YOUR GENDER:
+        <select id="gender" name="gender" placeholder="Enter your gender"  ><br>
+        
+            <option value="Male">Select Gender</option>
+           <option value="M">Male</option>
+           <option value="F">Female</option> </select>
+        </div>
+    <div>
+           ENTER YOUR COLLEGE NAME:
+           <input id="college" name="college" placeholder="Enter your college name"  type="text"><br>
+        </div>
+    <div>
+           ENTER YOUR MAIL ID:
+           <input id="email" name="email" placeholder="Enter your email-id" type="email"><br>
+        </div>
+    <div>
+           ENTER YOUR MOBILE NUMBER:
+           <input id="mob" name="mob" placeholder="Enter your mobile number"  type="number"><br>
+        </div>
+    <div>
+           ENTER YOUR PASSWORD:
+           <input id="password" name="password" placeholder="Enter your password"  type="password"><br>
+        </div>
+    <div>
+           RE-ENTER YOUR PASSWORD:
+           <input id="cpassword" name="cpassword" placeholder="Conform Password"type="password"><br>
+        </div>
+           <input  type="submit" class="sub" value="sign up" class="btn btn-primary"/>
+           </form>
+</body>
+</html>
